076cbf31b5cf57ce17cabf441843849d37c5c8db
ShaoKuy <iqtanawad@gmail.com>
2024-03-27 13:45:31 +0700

    15.53

--- Assets/Script/Buildings/Building.cs
+++ Assets/Script/Buildings/Building.cs
@@ -29,6 +29,12 @@ public class Building : Structure
     [SerializeField] private float intoTheGround = 5f;
     public float IntoTheGround { get { return intoTheGround; } }
     
+    private float timer = 0f; //Constructing timer
+    public float Timer { get { return timer; } set { timer = value; } }
+    private float waitTime = 0.5f; //How fast it will be construct, higher is longer
+    public float WaitTime { get { return waitTime; } set { waitTime = value; } }
+
+    
     void Start()
     {
         
@@ -119,7 +125,4 @@ public class Building : Structure
         if (SelectionVisual != null)
             SelectionVisual.SetActive(flag);
     }
-    
-    
-    
 }
